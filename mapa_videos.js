eccc9e4011f2c2ed6db23de57181714560093fd1
Ignacio Verdi <ignacioverdi@users.noreply.github.com>
2026-07-16 09:41:12 -0300

    Actualizacion jue 16/07/2026

--- mapa_videos.js
+++ mapa_videos.js
@@ -70,6 +70,7 @@ window.MAPA_VIDEOS = {
   "10193": "",
   "10196": "https://youtu.be/9cVuDBm95AY",
   "10197": "https://youtu.be/OBH0JiyyomI",
+  "10198": "https://youtu.be/RGk6SEIKRUw",
   "10199": "https://youtu.be/T2LgRslOUAM",
   "10200": "https://youtu.be/4OvDr34fSFw"
 };
